700089acbb4abbe7e9eb876d5dbde0b24cb8b886
gustavoweb <contato@gustavoweb.com>
2025-05-21 07:32:02 -0300

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+//Chamada das dependencias
+const express = require('express');
+const http = require('http');
+
+//inicializar aplicação Express
+const app = express();
+const server = http.createServer(app);
+
+//Inicializar conexões
+const inicilizar = async () => {
+    try {
+        //Escutar o servidor na porta 3000
+        const PORT = 3000;
+        server.listen(PORT, () => {
+            console.log(`Servidor rodando na porta ${PORT}`);
+        });
+    } catch {
+        console.log('Erro ao inicializar o servidor:',error);
+        process.exit(1);
+    }
+};
